f94377714a183bfd39cb3eeef3541a24cee8a11b
silver takana <titmitna@gmail.com>
2022-08-20 11:14:55 +1200

    premake5

--- premake5.lua
+++ premake5.lua
@@ -3,8 +3,8 @@ project "ImGui"
 	language "C++"
     staticruntime "off"
 
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+	targetdir (outputOrigin .."/bin/" .. outputdir .. "/%{prj.name}")
+	objdir (outputOrigin .."/bin-int/" .. outputdir .. "/%{prj.name}")
 
 	files
 	{
